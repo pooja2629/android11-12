64f464315454e99ae2c48494309cbb4bd57be190
pooja2629 <pooja2629@users.noreply.github.com>
2017-12-15 11:30:59 +0530

    initial commit

--- materialdesign/src/main/java/com/example/materialdesign/MainActivity.java
+++ materialdesign/src/main/java/com/example/materialdesign/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.pooja.myfirstapp;
+package com.example.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,5 +11,3 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
-
-
